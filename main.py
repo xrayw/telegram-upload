e9e9d66863f9259aa0c6a440c20a88be85c86137
f <nuonuo.mingfei@outlook.com>
2025-08-19 09:53:12 +0700

    init

---
+++ main.py
@@ -0,0 +1,105 @@
+import os
+import time
+import json
+# from tqdm import tqdm
+from telethon import TelegramClient
+from pathlib import Path
+from tkinter import Tk, filedialog
+
+MB = 1024 * 1024
+ME = "me"
+
+
+async def upload(files):
+    # await client.send_message('me', 'Hello! Talking to you')
+
+    print()
+    for i, filename in enumerate(files):
+        path = Path(filename)
+        # if not path.exists():
+        #     print(f"File {filename} does not exist.")
+        #     continue
+        # size = os.path.getsize(filename)
+        # with tqdm(
+        #     total=round(size/MB, 2),
+        #     unit='M',
+        #     desc=f"[{i + 1:3}:{len(files)}]",
+        # ) as pbar:
+        #     await client.send_file(
+        #         ME,
+        #         filename,
+        #         caption=filename,
+        #         supports_streaming=True,
+        #         progress_callback=lambda current, total: pbar.update(round(current/MB-pbar.n, 2)),
+        #         silient=True,
+        #         part_size=MB*10
+        #     )
+
+        start, upload_bytes = time.time(), 0
+
+        def callback(current, total):
+            # nonlocal upload_bytes
+            nonlocal start, upload_bytes
+            end = time.time()
+            cost = end - start
+            start = end
+
+            speed = (current - upload_bytes) / MB / cost
+
+            upload_bytes = current
+
+            print(f"\033[K {i:4}:{len(files)} {path.name[-50:]:>50.50} {total/MB:6.2f}M {(current / total) * 100:6.2f}% {speed:6.2f}M/s", end="\r")
+
+        await client.send_file(
+            ME,
+            filename,
+            caption=filename,
+            supports_streaming=True,
+            progress_callback=callback,
+            silient=True,
+            part_size=MB * 20,
+        )
+        print()
+
+
+
+if __name__ == "__main__":
+    HOME_DIR = os.path.expanduser("~")
+
+    after = None
+    if not os.path.exists(HOME_DIR + '/.tg_cache'):
+        appid = input("  Please input the appid:")
+        apphash = input("Please input the apphash:")
+
+        def saveid():
+            with open(HOME_DIR + '/.tg_cache', 'w') as f:
+                json.dump({'appid': appid, 'apphash': apphash}, f)
+            print("Saved appid and apphash to ~/.tg_cache")
+
+        after = saveid
+    else:
+        with open(HOME_DIR + '/.tg_cache', 'r') as f:
+            data = json.loads(f.read())
+            appid = data.get('appid')
+            apphash = data.get('apphash')
+
+    root = Tk()
+    root.withdraw()
+    root.attributes("-topmost", True)
+    try:
+        files = filedialog.askopenfilenames(parent=root)
+    finally:
+        root.destroy()
+    root.update()
+
+    if not files:
+        print("No files selected.")
+        exit(1)
+
+    client = TelegramClient("upload", appid, apphash)
+    client.start()
+    with client:
+        client.loop.run_until_complete(upload(files))
+
+    if after:
+        after()
