8056452431ad4b4635d92b48e712ce7f67900c33
f <nuonuo.mingfei@outlook.com>
2025-08-23 00:05:06 +0700

    log and reupload if failed

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from telethon import TelegramClient
 MB = 1024 * 1024
 ME = "me"
 CLEAR_PREV_LINE = "\033[F\033[K"
+FAILED_JSON = 'failed.json'
 
 
-async def upload(files, concurrency: int = 5):
+async def upload(files, concurrency: int = 5) -> list[str]:
     print(f"total: {len(files)}")
     print("done : 0")
 
@@ -37,7 +38,9 @@ async def upload(files, concurrency: int = 5):
         nonlocal rendered_cnt
         rendered_cnt = len(ongoing)
 
-    async def upload_one(filepath):
+    async def upload_one(idx, filepath):
+        """return filepath if failed """
+
         filename = os.path.split(filepath)[1]
         start, upload_bytes = time.time(), 0
 
@@ -60,6 +63,9 @@ async def upload(files, concurrency: int = 5):
 
         async with semaphore:
             try:
+                # if idx & 1 == 0:
+                #     raise Exception('Simulated upload failure for testing purposes.')
+
                 ongoing.append([filepath, ''])
 
                 await client.send_file(
@@ -70,14 +76,20 @@ async def upload(files, concurrency: int = 5):
                     progress_callback=callback,
                     silient=True,
                 )
+            except Exception:
+                return filepath
             finally:
                 t = get_ongoing_file_item(filepath)
                 if t:
                     del ongoing[t[0]]
 
-    for task in asyncio.as_completed([upload_one(filepath) for filepath in files]):
+    failed = []
+    for task in asyncio.as_completed([upload_one(i, filepath) for i, filepath in enumerate(files)]):
         try:
-            await task
+            fp = await task
+            if fp:
+                failed.append(fp)
+                continue
             done_cnt += 1
 
             clear_ongoing()
@@ -87,6 +99,7 @@ async def upload(files, concurrency: int = 5):
             render_ongoing_files()
         except Exception:
             pass
+    return failed
 
 
 def clear_prev_line():
@@ -94,6 +107,10 @@ def clear_prev_line():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload files to Telegram using Telethon.")
+    parser.add_argument('-c', '--count', type=int, default=5, help='Number of concurrent uploads (default: 5)')
+    args = parser.parse_args()
+
     HOME_DIR = os.path.expanduser("~")
 
     after = None
@@ -113,28 +130,53 @@ if __name__ == "__main__":
             appid = data.get("appid")
             apphash = data.get("apphash")
 
+    files = None
+    if os.path.exists(FAILED_JSON):
+        try:
+            with open(FAILED_JSON, 'r') as f:
+                failed = json.load(f)
+                if failed:
+                    yn = input('Do you want continue to upload failed files? (y/n):').strip().lower()
+                    if yn == 'y':
+                        files = failed
+                    else:
+                        print(f'You need to delete `{FAILED_JSON}` first.')
+                        exit(0)
+        except Exception:
+            pass
+
     root = Tk()
     root.withdraw()
     root.attributes("-topmost", True)
-    try:
-        files = filedialog.askopenfilenames(parent=root)
-    finally:
-        root.destroy()
-    root.update()
+
+    reupload = True if files else False
+    if not reupload:
+        try:
+            files = filedialog.askopenfilenames(parent=root)
+        finally:
+            root.destroy()
+        root.update()
 
     if not files:
         print("No files selected.")
         exit(1)
 
-    parser = argparse.ArgumentParser(description="Upload files to Telegram using Telethon.")
-    parser.add_argument('-c', '--count', type=int, default=5, help='Number of concurrent uploads (default: 5)')
-    args = parser.parse_args()
-
     client = TelegramClient("upload", appid, apphash)
     client.start()
     with client:
         try:
-            client.loop.run_until_complete(upload(files, args.count))
+            failed = client.loop.run_until_complete(upload(files, args.count))
+            if failed:
+                print(f"Failed to upload {len(failed)} files. See '{FAILED_JSON}' for details.")
+                for f in failed:
+                    print(os.path.split(f)[1])
+
+                with open(FAILED_JSON, 'w') as f:
+                    json.dump(failed, f, indent=2)
+            else:
+                if reupload:
+                    os.remove(FAILED_JSON)
+                    print(f"All files reuploaded successfully. '{FAILED_JSON}' was removed.")
         except Exception as e:
             print(f"An error occurred: {e}")
 
