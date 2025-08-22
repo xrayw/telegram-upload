5aa9bc949ca7e6c238a628b8fe38321288829dd5
f <nuonuo.mingfei@outlook.com>
2025-08-22 09:29:25 +0700

    Uploading multiple files concurrently

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
 import os
+import sys
 import time
 import json
-from telethon import TelegramClient
-from pathlib import Path
+import asyncio
 from tkinter import Tk, filedialog
+from telethon import TelegramClient
 
 MB = 1024 * 1024
 ME = "me"
+CLEAR_PREV_LINE = "\033[F\033[K"
 
 
 async def upload(files):
-    print()
-    for i, filepath in enumerate(files):
-        path = Path(filepath)
-        filename = path.name
+    print(f"total: {len(files)}")
+    print("done : 0")
+
+    semaphore = asyncio.Semaphore(5)
+    ongoing: list[list[str]] = []
+    done_cnt = 0
+    rendered_cnt = 0
+
+    def get_ongoing_file_item(filepath) -> tuple[int, list[str]] | None:
+        for idx, f in enumerate(ongoing):
+            if f[0] == filepath:
+                return (idx, f)
+        return None
+
+    def clear_ongoing():
+        for _ in range(rendered_cnt):
+            clear_prev_line()
+
+    def render_ongoing_files():
+        for f in ongoing:
+            print(f[1])
+        nonlocal rendered_cnt
+        rendered_cnt = len(ongoing)
+
+    async def upload_one(filepath):
+        filename = os.path.split(filepath)[1]
         start, upload_bytes = time.time(), 0
 
         def callback(current, total):
@@ -25,41 +49,68 @@ async def upload(files):
             speed = (current - upload_bytes) / MB / cost
 
             upload_bytes = current
+            tip = f"{filename[-50:]:>50.25} {current / MB:6.2f}/{total / MB:6.2f}M {(current / total) * 100:6.2f}% {speed:6.2f}M/s"
+            f = get_ongoing_file_item(filepath)
+            if f:
+                f[1][1] = tip
 
-            print(f"\033[K {i:4}:{len(files)} {filename[-50:]:>50.50} {total/MB:6.2f}M {(current / total) * 100:6.2f}% {speed:6.2f}M/s", end="\r")
+            clear_ongoing()
+            render_ongoing_files()
 
-        await client.send_file(
-            ME,
-            filepath,
-            caption=filename,
-            supports_streaming=True,
-            progress_callback=callback,
-            silient=True,
-            part_size=MB * 20,
-        )
-        print()
+        async with semaphore:
+            try:
+                ongoing.append([filepath, ''])
 
+                await client.send_file(
+                    ME,
+                    filepath,
+                    caption=filename,
+                    supports_streaming=True,
+                    progress_callback=callback,
+                    silient=True,
+                )
+            finally:
+                t = get_ongoing_file_item(filepath)
+                if t:
+                    del ongoing[t[0]]
+
+    for task in asyncio.as_completed([upload_one(filepath) for filepath in files]):
+        try:
+            await task
+            done_cnt += 1
+
+            clear_ongoing()
+            clear_prev_line()
+
+            print(f"done : {done_cnt}")
+            render_ongoing_files()
+        except Exception:
+            pass
+
+
+def clear_prev_line():
+    sys.stdout.write(CLEAR_PREV_LINE)
 
 
 if __name__ == "__main__":
     HOME_DIR = os.path.expanduser("~")
 
     after = None
-    if not os.path.exists(HOME_DIR + '/.tg_cache'):
-        appid =   input("  Please input the appid:")
+    if not os.path.exists(HOME_DIR + "/.tg_cache"):
+        appid = input("  Please input the appid:")
         apphash = input("Please input the apphash:")
 
         def saveid():
-            with open(HOME_DIR + '/.tg_cache', 'w') as f:
-                json.dump({'appid': appid, 'apphash': apphash}, f)
+            with open(HOME_DIR + "/.tg_cache", "w") as f:
+                json.dump({"appid": appid, "apphash": apphash}, f)
             print("Saved appid and apphash to ~/.tg_cache")
 
         after = saveid
     else:
-        with open(HOME_DIR + '/.tg_cache', 'r') as f:
+        with open(HOME_DIR + "/.tg_cache", "r") as f:
             data = json.loads(f.read())
-            appid = data.get('appid')
-            apphash = data.get('apphash')
+            appid = data.get("appid")
+            apphash = data.get("apphash")
 
     root = Tk()
     root.withdraw()
@@ -77,7 +128,10 @@ if __name__ == "__main__":
     client = TelegramClient("upload", appid, apphash)
     client.start()
     with client:
-        client.loop.run_until_complete(upload(files))
+        try:
+            client.loop.run_until_complete(upload(files))
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
     if after:
         after()
